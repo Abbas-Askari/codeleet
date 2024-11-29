d4e91ed3f80b0b9bc64fa0dc42881456e31f1d5a
Abbas-Askari <abbasaskari797@gmail.com>
2024-11-29 15:42:18 +0500

    moved backed to aws

--- constants.js
+++ constants.js
@@ -2,4 +2,5 @@ export const MAX_LOGS = 100;
 export const EXECUTION_TIMEOUT = 100;
 
 // export const BACKEND_URL = "http://localhost:3000/";
-export const BACKEND_URL = "https://lime-zany-moth.cyclic.app/";
+// export const BACKEND_URL = "https://lime-zany-moth.cyclic.app/";
+export const BACKEND_URL = "http://3.95.169.8/";
