e02bc0670f069552f2046a0406fadaee7495b0e3
Abbas-Askari <abbasaskari797@gmail.com>
2024-11-30 13:20:38 +0500

    change backend to heroku

--- constants.js
+++ constants.js
@@ -3,4 +3,4 @@ export const EXECUTION_TIMEOUT = 100;
 
 // export const BACKEND_URL = "http://localhost:3000/";
 // export const BACKEND_URL = "https://lime-zany-moth.cyclic.app/";
-export const BACKEND_URL = "http://3.95.169.8/";
+export const BACKEND_URL = "https://codeleet-back-dae7f723833d.herokuapp.com/";
